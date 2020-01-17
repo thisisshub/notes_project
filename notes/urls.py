5d970bcbccda6d154f265a5e823856312efc99ab
thisisshub <thisisshub@gmail.com>
2020-01-17 14:43:31 +0530

    new updated commit

--- notes/urls.py
+++ notes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='notes-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-post'),
     path('notes/new/', PostCreateView.as_view(), name='notes-create'),
     path('notes/<int:pk>', PostDetailView.as_view(), name='notes-detail'),
     path('notes/<int:pk>/update/', PostUpdateView.as_view(), name='notes-update'),
