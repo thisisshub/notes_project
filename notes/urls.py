1cccd869f077f7edabdd6931e7cffdbea4c9a09a
Shubhashish <shub@localhost.localdomain>
2020-02-22 23:01:29 +0530

    added syllabus

--- notes/urls.py
+++ notes/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='notes-home'),
     path('notes/new/', PostCreateView.as_view(), name='notes-create'),
     path('filter/', FilterView.as_view(filterset_class=NotesFilter, template_name='notes/templates/notes/notes/notes_model_filter.html'), name='notes-filter'),
+    path('about/', views.syllabus, name='syllabus'),
     path('notes/<int:pk>', PostDetailView.as_view(), name='notes-detail'),
     path('notes/<int:pk>/update/', PostUpdateView.as_view(), name='notes-update'),
     path('notes/<int:pk>/delete/', PostDeleteView.as_view(), name='notes-delete'),
